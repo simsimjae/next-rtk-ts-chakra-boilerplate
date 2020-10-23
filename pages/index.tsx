bee9d2325af5e519b863edab1fa4f0cc70e1ead2
simsimjae <191429@ssg.com>
2020-10-23 13:59:43 +0900

    update createEntityAdapter

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,15 @@
-import { Button, Heading, VStack } from '@chakra-ui/core';
-import React from 'react';
+import { Button, Code, Heading, VStack } from '@chakra-ui/core';
+import React, { useRef } from 'react';
 import useStore from '../src/hooks/useStore';
+import { bookAdded, booksReceived, bookUpdated } from '../src/store/slices/testEntitySlice';
 import { decrement, increment, incrementAsync } from '../src/store/slices/testSlice';
+import { booksSelectors } from '../src/store/store';
 
 interface Props {}
 
 const MainPage = (props: Props) => {
   const store = useStore();
+  const id = useRef(0);
 
   return (
     <div>
@@ -15,6 +18,23 @@ const MainPage = (props: Props) => {
         <Button onClick={() => store.dispatch(increment(3))}>Increment +3</Button>
         <Button onClick={() => store.dispatch(decrement(3))}>Decrement -3</Button>
         <Button onClick={() => store.dispatch(incrementAsync(3))}>Increment +3 After 1 second</Button>
+        <Heading>Books</Heading>
+        <Code>{JSON.stringify(booksSelectors.selectEntities(store.rootState))}</Code>
+        <Button onClick={() => store.dispatch(bookAdded({ id: id.current++, title: 'New Book' }))}>Add New Book</Button>
+        <Button onClick={() => store.dispatch(bookUpdated({ id: id.current - 1, changes: { title: 'Updated Book' } }))}>Update Last Book</Button>
+        <Button
+          onClick={() =>
+            store.dispatch(
+              booksReceived([
+                { id: id.current++, title: 'New Book 1' },
+                { id: id.current++, title: 'New Book 2' },
+                { id: id.current++, title: 'New Book 3' },
+              ]),
+            )
+          }
+        >
+          set books from api response
+        </Button>
       </VStack>
     </div>
   );
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,10 +1,14 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import logger from 'redux-logger';
+import testEntitySlice, { testEntityAdapter } from './slices/testEntitySlice';
 import testSlice from './slices/testSlice';
 
 const rootReducer = combineReducers({
   test: testSlice.reducer,
+  testEntity: testEntitySlice.reducer,
 });
+
+export const booksSelectors = testEntityAdapter.getSelectors<RootState>(state => state.testEntity);
 
 const store = configureStore({ reducer: rootReducer, middleware: getDefaultMiddleware => getDefaultMiddleware().concat(logger) });
 
